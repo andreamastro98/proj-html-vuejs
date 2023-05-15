ff6496a07fe1b2facee65f55d73227fef8dc9f7c
andreamastro98 <andreamastrovito98@gmail.com>
2023-05-15 16:18:59 +0200

    Sezione newslatter conpletata

--- src/components/NewsLetterComp.vue
+++ src/components/NewsLetterComp.vue
@@ -6,16 +6,21 @@
 
 <template>
     <div class="bg">
-        <div class="container-3 d-flex justify-content-between">
+        <div class="container-4 d-flex justify-content-between">
             <div class="width">
                 <span class="small-green-text">newsletter</span>
                 <div class="my-3">            
                     <span class="highlighted-text-2 white">Know</span>
                     <span class="white"> First</span>                                
                 </div>
+                <p>Follow closely and receivecontent aboutour company and the news of the current market.</p>
             </div>
             <div class="width">
-                
+                <input class="form-control form-control-lg mt-3 mb-4" type="text" placeholder="name" aria-label=".form-control-lg example">
+
+                <input class="form-control form-control-lg mb-4" type="email" placeholder="email" aria-label=".form-control-lg example">
+
+                <button type="button" class="btn btn-1">SUBSCRIBE</button>
             </div>
         </div>
     </div>
@@ -29,7 +34,27 @@
     background-color: #111117;
 }
 
+.container-4{
+    padding: 7rem 0 7rem 0;
+    width: 50%;
+    margin: 0 auto;
+}
+
 .width{
     width: 45%;
+}
+
+p{
+    color: #bcbdbd;
+    font-size: 17px;
+}
+
+.form-control{
+    background-color: #1D1D23;
+    border: #1D1D23;
+}
+
+.form-control::placeholder{
+    color: #bcbdbd91;
 }
 </style>
